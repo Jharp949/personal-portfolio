28f6d29148da2132622e977ba7d3794c750d86e0
James Harper <jharp949@gmail.com>
2023-07-23 19:19:13 -0700

    Changes to be committed: 	modified:   views/layouts/header.js

--- views/layouts/header.js
+++ views/layouts/header.js
@@ -7,13 +7,13 @@ class Header extends HTMLElement
     connectedCallback() {
         this.innerHTML = `
         <header>
-            <a href="../../index.html"><img class="logo" src="../../public/images/logo.png" alt="logo"></a>
+            <a href="./index.html"><img class="logo" src="./public/images/logo.png" alt="logo"></a>
             <nav>
                 <ul class="nav-links">
-                    <li><a href="../../index.html">Home</a></li>
-                    <li><a href="../about.html">About</a></li>
-                    <li><a href="../projects.html">Projects</a></li>
-                    <li><a href="../resume.html">Resume</a></li>
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./views/about.html">About</a></li>
+                    <li><a href="./views/projects.html">Projects</a></li>
+                    <li><a href="./views/resume.html">Resume</a></li>
                 </ul>
             </nav>
         </header>
