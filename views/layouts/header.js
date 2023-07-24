574a0b935e565648f5b28ff1c84b8ca4b1157b9e
James Harper <jharp949@gmail.com>
2023-07-23 19:23:42 -0700

    Changes to be committed: 	modified:   views/layouts/header.js

--- views/layouts/header.js
+++ views/layouts/header.js
@@ -10,10 +10,10 @@ class Header extends HTMLElement
             <a href="./index.html"><img class="logo" src="public/images/logo.png" alt="logo"></a>
             <nav>
                 <ul class="nav-links">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="views/about.html">About</a></li>
-                    <li><a href="views/projects.html">Projects</a></li>
-                    <li><a href="views/resume.html">Resume</a></li>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/views/about.html">About</a></li>
+                    <li><a href="/views/projects.html">Projects</a></li>
+                    <li><a href="/views/resume.html">Resume</a></li>
                 </ul>
             </nav>
         </header>
